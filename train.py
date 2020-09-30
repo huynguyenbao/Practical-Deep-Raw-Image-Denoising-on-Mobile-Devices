183474eb3dc4828da8e0c8970301ae3956081978
huynguyenbao <huynguyenbao@outlook.com>
2020-09-30 23:20:18 +0700

    Clean code in train.py

--- train.py
+++ train.py
@@ -1,44 +1,39 @@
+from model import DenoiseNetwork
 import tensorflow as tf
-import sys
-import time
+from preprocess import preprocess_data
+import argparse 
+import os
 
 def loss_function(y_ground_truth, y_pred):
   return tf.reduce_sum(tf.reduce_mean(tf.abs(y_pred - y_ground_truth), axis=0))
 
-def train_step(model, optimizer, clean_img, noisy_img):
 
-  variables = model.variables
-  with tf.GradientTape(watch_accessed_variables=False) as tape:
+if __name__ == "__main__":
     
-    tape.watch(variables)
-    y_pred = model(noisy_img)
-    loss = loss_function(clean_img, y_pred)
+    parser = argparse.ArgumentParser()
 
-  grads = tape.gradient(loss, variables)
-  optimizer.apply_gradients(zip(grads, variables))
+    # Input Parameters
+    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--checkpoints_folder', type=str, default="weights//ckpt")
+    parser.add_argument('--pretrain_dir', type=str)
 
-  return loss
+    config = parser.parse_args()
 
-def progress(epoch, trained_sample ,total_sample, bar_length=25, loss=0, message=""):
-  percent = float(trained_sample) / float(total_sample)
-  hashes = '#' * int(tf.round(percent * bar_length))
-  spaces = ' ' * (bar_length - len(hashes))
-  sys.stdout.write("\rEpoch {0}: [{1}] {2}%  ----- Loss: {3}".format(epoch, hashes + spaces, int(round(percent * 100)), float(loss)) + message)
-  sys.stdout.flush()
+    # Check GPU
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(config.gpu)
 
-def fit(model, lr, clean_imgs, noisy_imgs, epochs, checkpoints_folder):
-  history = []
-  optimizer = tf.keras.optimizers.Adam(lr)
-  for e in range(epochs):
-    start = time.time()
-    total_loss = [1]
-    for ite, (clean_img, noisy_img) in tf.data.Dataset.zip((clean_imgs, noisy_imgs)).enumerate():
-      loss = train_step(model, optimizer, clean_img, noisy_img)
-      progress(e+1, (ite+1), len(clean_imgs), loss=loss)
-      total_loss.append(loss)
-    history.append(sum(total_loss) / len(total_loss))
-    end = time.time()
-    print("\nEpoch {0}: ---------- Avg Loss: {1}, time exection: {2}".format(e+1, sum(total_loss) / len(total_loss), end - start))
-    if (e+1) % 10 == 0:
-      model.save_weights(checkpoints_folder)
-  return history
+    # Get Data
+    clean_dataset, noisy_dataset = preprocess_data()
+
+    # Create Model
+    model = DenoiseNetwork()
+    model.build((None,512,512,3))
+    if config.pretrain_dir:
+        model.load_weights(config.pretrain_dir)
+
+    # Train Model
+    opt = tf.keras.optimizers.Adam(config.lr)
+    model.compile(optimizer=opt, loss=loss_function, metrics=[loss_function])
+    model.fit(tf.data.Dataset.zip((noisy_dataset, clean_dataset)), epochs=config.epochs)
